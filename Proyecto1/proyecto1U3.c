e5388b322b5bbd42921b94d999e9d125f3275d07
EBambi <esteban.gonzalezg@upb.edu.co>
2021-04-25 15:00:10 -0500

    Proyecto1Full

--- Proyecto1/proyecto1U3.c
+++ Proyecto1/proyecto1U3.c
@@ -9,16 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-    int pfd[2], psn[2]; /* Pipe file descriptors */
-    char buf[BUF_SIZE], msgMin[BUF_SIZE], msgMay[BUF_SIZE];
+    int pfd[2], psn[2];
+    char bufP[BUF_SIZE], bufS[BUF_SIZE];
     ssize_t numRead;
 
-    if (pipe(pfd) == -1){ /* Create the pipe */
+    if (pipe(pfd) == -1){
         perror("pipe pfd");
         exit(EXIT_FAILURE);
     }
 
-    if (pipe(psn) == -1){ /* Create the pipe */
+    if (pipe(psn) == -1){
         perror("pipe psn");
         exit(EXIT_FAILURE);
     }
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     case -1:
         perror("fork");
         exit(EXIT_FAILURE);
-    case 0: /* Child  - reads from pipe */
+    case 0: /* Hijo */
         if (close(pfd[1]) == -1)
         {
             perror("close write - child");
@@ -39,13 +39,15 @@ int main(int argc, char *argv[])
             perror("close read - child");
             exit(EXIT_FAILURE);
         }
-        numRead = read(pfd[0], buf, BUF_SIZE);
-        int i = 0;
-        while(buf[i]){
-            msgMay[i] = toupper(buf[i]);
-            i++;
+        for(;;){
+            numRead = read(pfd[0],bufS,BUF_SIZE);
+            int i = 0;
+            while(bufS[i]){
+                bufS[i] = toupper(bufS[i]);
+                i++;
+            }
+            write(psn[1],bufS,BUF_SIZE);
         }
-        write(psn[1],msgMay,BUF_SIZE);
         if (close(pfd[0]) == -1)
         {
             perror("close");
@@ -56,8 +58,7 @@ int main(int argc, char *argv[])
             perror("close");
             exit(EXIT_FAILURE);
         }
-        exit(EXIT_SUCCESS);
-    default: /* Parent - writes to pipe */
+    default: /* Padre */
         if (close(pfd[0]) == -1)
         {
             perror("close read - parent");
@@ -68,15 +69,17 @@ int main(int argc, char *argv[])
             perror("close write - parent");
             exit(EXIT_FAILURE);
         }
-        scanf("%s",buf);
-        int j = 0;
-        while(buf[j]){
-            msgMin[j] = tolower(buf[j]);
-            j++;
+        for(;;){
+            scanf("%s",bufP);
+            int j = 0;
+            while(bufP[j]){
+                bufP[j] = tolower(bufP[j]);
+                j++;
+            }
+            write(pfd[1],bufP,BUF_SIZE);
+            read(psn[0],bufS,BUF_SIZE);
+            printf("Texto Pipe: %s\n", bufS);
         }
-        write(pfd[1], msgMin, BUF_SIZE);
-        read(psn[0],buf,BUF_SIZE);
-        printf("Texto final: %s\n", buf);
         if (close(pfd[1]) == -1)
         {
             perror("close");
@@ -87,7 +90,5 @@ int main(int argc, char *argv[])
             perror("close");
             exit(EXIT_FAILURE);
         }
-        wait(NULL);
-        exit(EXIT_SUCCESS);
     }
 }
